30bc1a7d591736460ebb63aa11591fc983fb0463
maciej-galuszka <maciej.galuszka01@gmail.com>
2025-12-30 21:58:06 +0100

    feat: add results component

---
+++ src/components/Results.tsx
@@ -0,0 +1,27 @@
+type ResultsProps = {
+  amount: number;
+  amountConverted: number;
+  fromCurrency: string;
+  toCurrency: string;
+  isLoading: boolean;
+};
+
+export function Results({
+  amount,
+  amountConverted,
+  fromCurrency,
+  toCurrency,
+  isLoading,
+}: ResultsProps) {
+  return (
+    <div className="results">
+      {isLoading ? (
+        <p>
+          Loading...<span className="loader"></span>
+        </p>
+      ) : (
+        <p>{`${amount} ${fromCurrency} = ${amountConverted} ${toCurrency}`}</p>
+      )}
+    </div>
+  );
+}
